98d843643011e63e21765b0992f7050500748e5e
stanley <stanley.njuguna@student.moringaschool.com>
2023-12-17 13:16:13 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(defaultActivity = 'roller-skate' ){
+    return `This Saturday, I want to ${defaultActivity}!`;
+}
+saturdayFun("bathe my dog!");
+
+function mondayWork(defaultActivity = 'go to the office'){
+    return `This Monday, I will ${defaultActivity}.`;
+}
+mondayWork("work from home");
+
+
+function wrapAdjective(wrap = "*") {
+    return function(word = "special") {
+        return `You are ${wrap}${word}${wrap}!`;    }
+}
